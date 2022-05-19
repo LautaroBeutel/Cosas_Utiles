6287ca83a1b726e9cdf956a8e242ae3c58fdbdab
lautaro <lautarob52@mail.com>
2022-05-18 22:07:56 -0300

    comentarios agregados

--- Electronica/Registro_de_desplazamiento/74hc595_codigo/src/main.cpp
+++ Electronica/Registro_de_desplazamiento/74hc595_codigo/src/main.cpp
@@ -8,21 +8,23 @@
 //****************************************************************
 #include <Arduino.h>
 
-#define SRL_DATA 2
-#define SHIFT_CLOCK 3
-#define LATCH_CLOCK 4
+#define SRL_DATA 2          //Data Out PIN
+#define SHIFT_CLOCK 3       //Register CLK PIN
+#define LATCH_CLOCK 4       //Register ouput latch PIN
 
-void Escribir(byte DATA){
+void Escribir(byte DATA){       //Funcion para enviar info al 74hc595
   shiftOut(SRL_DATA, SHIFT_CLOCK, LSBFIRST, DATA);
-  digitalWrite(LATCH_CLOCK, LOW);
+
+  digitalWrite(LATCH_CLOCK, LOW);     //Latch para el registro de las salidas
   digitalWrite(LATCH_CLOCK, HIGH);
   digitalWrite(LATCH_CLOCK, LOW);
 }
 
 void setup() {
-  pinMode(SRL_DATA, OUTPUT);
+  pinMode(SRL_DATA, OUTPUT);      //declaracion de las salidas
   pinMode(SHIFT_CLOCK, OUTPUT);
   pinMode(LATCH_CLOCK, OUTPUT);
+
   pinMode(LED_BUILTIN, OUTPUT);
   
 }
