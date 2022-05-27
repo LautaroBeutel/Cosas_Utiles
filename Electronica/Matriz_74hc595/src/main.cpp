999f478965a5ba62e047968a4fae767b7ab530f8
lautaro <lautarob52@mail.com>
2022-05-27 19:12:17 -0300

    Matriz imprime correctamente

--- Electronica/Matriz_74hc595/src/main.cpp
+++ Electronica/Matriz_74hc595/src/main.cpp
@@ -17,47 +17,57 @@
 #define B_SHIFT_CLOCK 5
 #define B_LATCH_CLOCK 6
 
+#define INICIO_DELAY 1000
+#define SALTO_DELAY 5
+
+byte FILA[] = {B11111110, B11111101, B11111011, B11110111, B11101111, B11011111, B10111111, B01111111};
+byte X[] = {B10000001, B01000010, B00100100, B00011000, B00011000, B00100100, B01000010, B10000001};
+
+byte Corazon_datos[] = {
+  B00000000,
+  B01100110,
+  B11111111,
+  B11111111,
+  B01111110,
+  B00111100,
+  B00011000,
+  B00000000
+};
+
+byte Mensaje_datos[] = {
+  B00000000,
+  B00000000,
+  B01010100,
+  B01110100,
+  B01010100,
+  B00000000,
+  B00000100,
+  B00000000
+};
+
 int intervalo = 1000;
 unsigned int tiempo_1 = 0;
 
 void Escribir(int pin_data, int pin_clock, int pin_latch, byte DATA){       //Funcion para enviar info al 74hc595
 
   
-  shiftOut(pin_data, pin_clock, LSBFIRST, DATA);
+  shiftOut(pin_data, pin_clock, MSBFIRST, DATA);
 
   digitalWrite(pin_latch, LOW);     //Latch para el registro de las salidas
   digitalWrite(pin_latch, HIGH);
   digitalWrite(pin_clock, LOW);
 }
 
-void LETRA_L(){
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B11111110);
-
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B11111101);
-
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B11111011);
-
-
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B11110111);
-
-
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B11101111);
-
-
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B11011111);
-
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B10111111);
-
-
-  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, B11111111);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, B01111111);
+void Imprimir(byte Dato[], byte fila[]){
+  for (int8_t i = 0; i <= 7 ; i++)
+  {
+    Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, 0x00);
+    Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, 0xFF);
+    Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, Dato[i]);
+    Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, fila[i]);
+    //delay(SALTO_DELAY);
+  }
+  
 }
 
 void setup() {
@@ -69,11 +79,12 @@ void setup() {
   pinMode(B_SHIFT_CLOCK, OUTPUT);
   pinMode(B_LATCH_CLOCK, OUTPUT);
 
-  /* Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, 0xFF);
-  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, 0x00); */
+  Escribir(A_SRL_DATA, A_SHIFT_CLOCK, A_LATCH_CLOCK, 0xFF);
+  Escribir(B_SRL_DATA, B_SHIFT_CLOCK, B_LATCH_CLOCK, 0x00);
+  delay(INICIO_DELAY);
 
 }
 
 void loop() {
-  LETRA_L();
+  Imprimir(X, FILA);
 }
